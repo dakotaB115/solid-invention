efb16b40ea2a47ec2915d1181f0634ecd0cb5110
Dakota Brown <jinsai.inc@gmail.com>
2018-10-22 10:51:03 -0500

    Initial Commit

---
+++ FirebaseTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTesting
+//
+//  Created by Dakota Brown on 10/22/18.
+//  Copyright © 2018 Dakota Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
